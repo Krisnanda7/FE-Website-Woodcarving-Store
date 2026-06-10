b81f48d0194f70768fc1cfce9787c071c0aacc76
Krisnanda <baguskrisna424@gmail.com>
2026-06-10 09:44:57 +0800

    feat: added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import CartSidebar from "./components/cart/CartSideBar";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -50,6 +51,7 @@ export default function RootLayout({
         />
 
         {children}
+        <Analytics />
         <CartSidebar />
       </body>
     </html>
